3dd1ec7f21001396627fc055f94da9d654f27585
Batkhuslen Boldbaatar <Batkhuslen.Boldbaatar@elev.ga.ntig.se>
2023-10-06 10:04:55 +0200

    initial

---
+++ Tamagotchi_spel/Program.cs
@@ -0,0 +1,13 @@
+﻿using Namespace;
+
+Console.WriteLine("Välkommen till Tamagotchi");
+
+Tamagotchi Vitamin = new Tamagotchi();
+
+Console.WriteLine ("Välj ett namn för din tamagotchi");
+
+Vitamin.name = Console.ReadLine();
+
+
+
+
---
+++ Tamagotchi_spel/tamagotchi.cs
@@ -0,0 +1,15 @@
+using System.Runtime.CompilerServices;
+
+namespace Namespace;
+public class Tamagotchi
+{
+    private int hunger;
+    private int boredom;
+    private List<string> words =new List<string>() {"Tjenare"};
+    private bool isAlive;
+    private Random generator;
+    public string name;
+}
+
+
+
